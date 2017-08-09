e22073bda48fb704c200dffe8fc72c74d53de4b0
Ha Nhat Lenh <lenh@ktc.local>
2017-08-09 16:35:16 +0900

    Commited from lenh at Wed, Aug 09, 2017  4:35:16 PM

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -29,6 +29,7 @@ angular.module('users').config(['$stateProvider',
         url: '/picture',
         templateUrl: 'modules/users/client/views/settings/change-profile-picture.client.view.html'
       })
+      // パスワード認証
       .state('authentication', {
         abstract: true,
         url: '/authentication',
@@ -42,6 +43,7 @@ angular.module('users').config(['$stateProvider',
         url: '/signin?err',
         templateUrl: 'modules/users/client/views/authentication/signin.client.view.html'
       })
+      // パスワードを忘れるとき
       .state('password', {
         abstract: true,
         url: '/password',
