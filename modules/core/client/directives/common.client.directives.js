9e8c5057773f7581967ba61f8a2e2f9047d59fd6
Ha Nhat Lenh <lenh@ktc.local>
2017-08-10 12:11:38 +0900

    Commited from lenh at Thu, Aug 10, 2017 12:11:38 PM

---
+++ modules/core/client/directives/common.client.directives.js
@@ -0,0 +1,41 @@
+'use strict';
+angular
+  .module('core')
+  .directive('a', preventClickDirective)
+  .directive('a', cardExpandDirective);
+
+//Prevent click if href="#"
+function preventClickDirective() {
+  var directive = {
+    restrict: 'E',
+    link: link
+  };
+  return directive;
+
+  function link(scope, element, attrs) {
+    if (attrs.href === '#') {
+      element.on('click', function (event) {
+        event.preventDefault();
+      });
+    }
+  }
+}
+
+//Card Collapse
+function cardExpandDirective() {
+  var directive = {
+    restrict: 'E',
+    link: link
+  };
+  return directive;
+
+  function link(scope, element, attrs) {
+    element.on('click', function () {
+      if (element.hasClass('card-expand-toggle')) {
+        // angular.element('body').toggleClass('sidebar-mobile-show');
+        element.parent().parent().find('.card-expand').toggleClass('open');
+      }
+      // element.find('i').toggleClass('r180');
+    });
+  }
+}
