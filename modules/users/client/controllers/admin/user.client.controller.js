d1ee70cb7e1b1cbcf1f9e818a9ce2fc759143fb6
Ha Nhat Lenh <lenh@ktc.local>
2017-08-10 20:24:49 +0900

    Commited from lenh at Thu, Aug 10, 2017  8:24:49 PM

--- modules/users/client/controllers/admin/user.client.controller.js
+++ modules/users/client/controllers/admin/user.client.controller.js
@@ -10,6 +10,8 @@ function UserController($scope, $state, Authentication, userResolve, dialog, toa
   if ($scope.user._id) {
     $scope.user.createdMM = moment($scope.user.created);
     $scope.user.role = (_.contains($scope.user.roles, 'admin')) ? '管理者' : '一般ユーザー';
+    $scope.user.isAdmin = _.contains($scope.user.roles, 'admin');
+    $scope.user.isUser = !_.contains($scope.user.roles, 'admin');
   }
   $scope.newPassword = '';
 
